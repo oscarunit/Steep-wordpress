b0e486b50fdd25b823d564f91724ed68690666da
YannQuillard <y38.quillard@gmail.com>
2020-11-16 16:22:17 +0100

    ajout header

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+function register_assets() {
+
+    // Déclarer style.css à la racine du thème
+    wp_enqueue_style( 
+        'style',
+        get_stylesheet_uri(), 
+        array(), 
+        '1.0'
+    );
+
+    //Déclarer reset.css
+    wp_enqueue_style( 
+        'reset', 
+        get_template_directory_uri() . '/css/reset.css',
+        array(), 
+        '1.0'
+    );
+  	
+    // Déclarer un autre fichier CSS
+    wp_enqueue_style( 
+        'home', 
+        get_template_directory_uri() . '/css/home.css',
+        array(), 
+        '1.0'
+    );
+
+    wp_enqueue_style( 
+        'header', 
+        get_template_directory_uri() . '/css/header.css',
+        array(), 
+        '1.0'
+    );
+}
+add_action( 'wp_enqueue_scripts', 'register_assets' );
--- header.php
+++ header.php
@@ -13,3 +13,13 @@
 <body <?php body_class(); ?>>
     
     <?php wp_body_open(); ?>
+    <!--HEADER-->
+    <header class="header">
+        <a href="index.php"><img src="wp-content\themes\steep\images\Logo_STEEP.svg" alt="logo STEEP" class="header__logo"></a>
+        <nav class="header__nav">
+            <li class="header__nav__link"><a href="">Activités de recherche <img src="wp-content\themes\steep\images\arrow.svg" alt="Flêche vers le bas" class="header__nav__link__arrow"></a></li>
+            <li class="header__nav__link"><a href="">Vous formez <img src="wp-content\themes\steep\images\arrow.svg" alt="Flêche vers le bas" class="header__nav__link__arrow"></a></li>
+            <li class="header__nav__link"><a href="">Qui sommes-nous ? <img src="wp-content\themes\steep\images\arrow.svg" alt="Flêche vers le bas" class="header__nav__link__arrow"></a></li>
+        </nav>
+        <img src="wp-content\themes\steep\images\rechercher.svg" alt="Recherche" class="header__search">
+    </header>
